cb278639e37ec8735feacdad430ba8ee3f8d835d
Aaron Graham <apj_graham@yahoo.co.uk>
2022-03-29 16:55:11 +0100

    Added InputValidation Unit tests

--- test/test_argparser.py
+++ test/test_argparser.py
@@ -11,21 +11,20 @@ from src.arg_parser import parse_arguments
 class TestArgParser(unittest.TestCase):
     """Unit Tests for the arg parser"""
 
+    @patch.object(sys, "argv", ["prog", "test_string1", "test_string2"])
     def test_arg_parser(self):
         """Parser returns two strings when given two strings"""
-        test_args = ["prog", "test_string1", "test_string2"]
         expected_result = {
             "banned_words_file": "test_string1",
             "document": "test_string2",
         }
-        with patch.object(sys, "argv", test_args):
-            args = parse_arguments()
-            self.assertIsInstance(args, Namespace)
+        args = parse_arguments()
+        self.assertIsInstance(args, Namespace)
 
-            self.assertTrue(hasattr(args, "banned_words_file"))
-            self.assertIsInstance(args.banned_words_file, str)
+        self.assertTrue(hasattr(args, "banned_words_file"))
+        self.assertIsInstance(args.banned_words_file, str)
 
-            self.assertTrue(hasattr(args, "document"))
-            self.assertIsInstance(args.document, str)
+        self.assertTrue(hasattr(args, "document"))
+        self.assertIsInstance(args.document, str)
 
-            self.assertDictEqual(expected_result, vars(args))
+        self.assertDictEqual(expected_result, vars(args))
